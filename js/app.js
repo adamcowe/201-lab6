9a864944ca2cc3df4a4602aadf4e8b0e57f2beac
ADAM S COWEN <adamcowe@amazon.com>
2019-03-22 14:21:41 -0700

    lab7 add comments to app.js

--- js/app.js
+++ js/app.js
@@ -16,12 +16,14 @@ function Stores(storeName, minCustHourly, maxCustHourly, avgCooksPerCust) {
   this.dailyTotal = 0; //start at zero
   Stores.all.push(this);
 }
+//calculate th erandom customers per hour
 Stores.prototype.custPerHourCalc = function() {
   for (var i = 0; i < hours.length; i++) {
     this.custPerHour.push(random(this.minCustHourly, this.maxCustHourly));
   }
 };
 //console.log(Stores.prototype.custPerHourCalc());
+//calculate the cookies per hour, single store
 Stores.prototype.cooksPerHourCalc = function() {
   this.custPerHourCalc();
   for (var i = 0; i < hours.length; i++) {
@@ -30,7 +32,7 @@ Stores.prototype.cooksPerHourCalc = function() {
     this.dailyTotal += hourUnit;
   }
 };
-
+//
 Stores.prototype.render = function() {
   this.cooksPerHourCalc();
   var trRow = document.createElement('tr');
@@ -51,21 +53,21 @@ Stores.prototype.render = function() {
 
   salesData.appendChild(trRow);
 };
-
+//add the Store details and create each store
 Stores.all = [];
 new Stores('Pike\'s Place', 23, 65, 6.3);
 new Stores('Sea Tac Airport', 3, 24, 1.2);
 new Stores('Seattle Center', 11, 38, 3.7);
 new Stores('Capitol Hill', 20, 38, 2.3);
 new Stores('Alki', 2, 16, 4.6);
-
+//??
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
+//create total variables
 var hourTotals = 0;
 var grandTotal = 0;
-
+//build and label the header, //loop the hours
 function buildHeader() {
   var trRow = document.createElement('tr');
   var thRow = document.createElement('th');
@@ -83,12 +85,13 @@ function buildHeader() {
 
   salesData.appendChild(trRow);
 }
+//build and label the footer, loop the totals
 function buildFooter() {
   var trRow = document.createElement('tr');
   var thRow = document.createElement('th');
   thRow.textContent = 'Hourly Totals for All Stores';
   trRow.appendChild(thRow);
-
+  //??
   for (var first = 0; first < hours.length; first++) {
     hourTotals = 0;
     for (var second = 0; second < Stores.all.length; second++){
@@ -105,7 +108,7 @@ function buildFooter() {
 
   salesData.appendChild(trRow);
 }
-
+//render the table and push to 'sales-data' table in HTML
 function renderTable() {
   salesData.innerHTML = '';
   buildHeader();
